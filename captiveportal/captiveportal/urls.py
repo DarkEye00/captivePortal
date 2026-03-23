189136b3f6e01e626d14d8f2730120634fe8e33b
DarkEye00 <josephodhiambo509@gmail.com>
2026-03-23 14:34:36 +0300

    sucess page and url configuration

--- captiveportal/captiveportal/urls.py
+++ captiveportal/captiveportal/urls.py
@@ -5,16 +5,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portal.urls')),
 ]
-```
-
----
-
-## Step 5 — Create Templates
-
-Create this folder structure:
-```
-captive-portal/
-└── templates/
-    └── portal/
-        ├── login.html
-        └── success.html
